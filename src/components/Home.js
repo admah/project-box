24ac70a486aea403cd9f16e7bfd408b6cf6cf038
Adam Murray <adam.murray@lifeway.com>
2018-11-28 13:30:06 -0600

    Add initial components. Add router and ui modules.

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "70px" }}>
+        This will be a display of projects and materials.
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/Materials.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Materials extends Component {
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "70px" }}>
+        This will be a display of materials.
+      </div>
+    );
+  }
+}
+
+export default Materials;
