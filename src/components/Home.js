94cc61b21598813db13224d015f457ac6e1c4517
Adam Murray <adam.murray@lifeway.com>
2018-12-04 15:07:11 -0600

    Update layout. Remove unusedd js file.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
 
 class Home extends Component {
   render() {
     return (
-      <div className="ui container" style={{ marginTop: "70px" }}>
-        <p>This will be a display of projects and materials.</p>
-      </div>
+      <Container text style={{ marginTop: "70px" }}>
+        <h1>Welcome to Project-Box!</h1>
+        <p>
+          This is a tool designed to help you manage all of those house projects
+          that you love to do.
+        </p>
+        <p>Simply log in or create an account, and start adding projects!</p>
+      </Container>
     );
   }
 }
