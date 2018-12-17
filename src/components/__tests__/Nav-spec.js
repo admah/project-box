a9d8e1d9ad794b78169b62cbd8f70321b17a29f5
Adam Murray <adam.murray@lifeway.com>
2018-12-16 21:30:59 -0600

    Add react-testing-library. Add prelim tests for Home and Nav components.

---
+++ src/components/__tests__/Nav-spec.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { cleanup, render } from "react-testing-library";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+import "jest-dom/extend-expect";
+import Nav from "../Nav";
+
+afterEach(cleanup);
+
+function renderWithRouter(
+  ui,
+  {
+    route = "/",
+    history = createMemoryHistory({ initialEntries: [route] })
+  } = {}
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    // adding `history` to the returned utilities to allow us
+    // to reference it in our tests (just try to avoid using
+    // this to test implementation details).
+    history
+  };
+}
+
+describe("Nav", () => {
+  test("it renders the container", () => {
+    const props = {
+      history: {},
+      match: {},
+      user: {}
+    };
+    const route = "/";
+    const { container } = renderWithRouter(<Nav {...props} />, { route });
+    expect(container).toBeDefined();
+  });
+});
