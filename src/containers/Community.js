75e6e2f8006d6cfc0b400b12172e956012a2544b
Adam Murray <adam.murray@lifeway.com>
2018-12-27 09:26:43 -0600

    Hide community for unauth users. Add user check to community routes.

--- src/containers/Community.js
+++ src/containers/Community.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { Redirect, Route, Switch } from "react-router-dom";
 import CommunityProjects from "../components/CommunityProjects";
 import CommunityProjectDisplay from "../components/CommunityProjectDisplay";
 
-const Community = () => (
+const mapStateToProps = state => {
+  return { user: { name: state.account.name, id: state.account.id } };
+};
+
+const Community = ({ user }) => (
   <React.Fragment>
     <Switch>
+      {!user.id && <Redirect to="/login" />}
       <Route exact path="/community/:tag?" component={CommunityProjects} />
       <Route
         path="/community/project/:projectId"
@@ -16,4 +23,8 @@ const Community = () => (
   </React.Fragment>
 );
 
-export default Community;
+Community.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps)(Community);
